73b9f4155d3c7236d5760ec9b69d543ece6bef78
manojnaik18 <122071370+manojnaik18@users.noreply.github.com>
2023-07-10 15:20:28 +0530

    SJFnon-preemptive.c

--- SJFnon-preemptive.c
+++ SJFnon-preemptive.c
@@ -38,3 +38,20 @@ int main() {
     
     return 0;
 }
+//output
+//Enter the number of processes: 4
+//Enter the arrival time for process P1: 2
+//Enter the burst time for process P1: 1
+//Enter the arrival time for process P2: 1
+//Enter the burst time for process P2: 5
+//Enter the arrival time for process P3: 4
+//Enter the burst time for process P3: 1
+//Enter the arrival time for process P4: 0
+//Enter the burst time for process P4: 6
+//P[4]    |       6       |       0
+//P[1]    |       5       |       4
+//P[3]    |       4       |       3
+//P[2]    |       12      |       7
+
+//Average waiting time = 3.500000
+//Average turnaround time = 6.750000[1] + Done  
