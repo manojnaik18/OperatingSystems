47de6bc4e368505a933188b6edbb8e78fe461198
manojnaik18 <122071370+manojnaik18@users.noreply.github.com>
2023-08-03 23:12:31 +0530

    Create Roundrobin.c

---
+++ Roundrobin.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main() {
+    int i, NOP, sum = 0, count = 0, y, quant, wt = 0, tat = 0, at[10], bt[10], temp[10];
+    float avg_wt, avg_tat;
+
+    printf("Total number of processes in the system: ");
+    scanf("%d", &NOP);
+
+    y = NOP; // Assign the number of processes to variable y
+
+    // Use a for loop to enter the details of each process
+    for (i = 0; i < NOP; i++) {
+         printf("Enter the arrival time for process P%d: ", i + 1);
+
+        scanf("%d", &at[i]);
+        printf("Enter the burst time for process P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+        temp[i] = bt[i]; // Store the burst time in temp array
+    }
+
+    printf("\nEnter the Time Quantum for the processes: ");
+    scanf("%d", &quant);
+
+    printf("\nProcess No\tBurst Time\tTAT\tWaiting Time");
+
+    for (sum = 0, i = 0; y != 0;) {
+        if (temp[i] <= quant && temp[i] > 0) {
+            sum = sum + temp[i];
+            temp[i] = 0;
+            count = 1;
+        } else if (temp[i] > 0) {
+            temp[i] = temp[i] - quant;
+            sum = sum + quant;
+        }
+
+        if (temp[i] == 0 && count == 1) {
+            y--;
+            printf("\nProcess No[%d]\t%d\t\t%d\t\t%d", i + 1, bt[i], sum - at[i], sum - at[i] - bt[i]);
+            wt = wt + sum - at[i] - bt[i];
+            tat = tat + sum - at[i];
+            count = 0;
+        }
+
+        if (i == NOP - 1) {
+            i = 0;
+        } else if (at[i + 1] <= sum) {
+            i++;
+        } else {
+            i = 0;
+        }
+    }
+
+    avg_wt = (float)wt / NOP;
+    avg_tat = (float)tat / NOP;
+
+    printf("\nAverage Turn Around Time: %f", avg_wt);
+    printf("\nAverage Waiting Time: %f", avg_tat);
+
+    return 0;
+}
